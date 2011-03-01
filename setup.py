4cc103708d9f64ac6f6c67d84acc80d1d1b06045
Adam Faulconbridge <afaulconbridge@googlemail.com>
2011-03-01 22:00:41 +0000

    corrected setuputils to distutils

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #python setup file
-#This is for setuputils to create packages for distribution
+#This is for distutils to create packages for distribution
 #Generally, this shouldnt be used directly for installation
 #and easy_install -U pyga should be used instead.
 #If you really must install from this, then run:
 #sudo python setup.py install
+
+#TODO write this file...
